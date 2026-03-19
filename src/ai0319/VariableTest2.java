5f5fc3055fe68a91d76aa0b808dafe671cb4ee0b
ratewolf <gilsu0622@gmail.com>
2026-03-19 10:46:11 +0900

    Initial Commit

---
+++ src/ai0319/VariableTest2.java
@@ -0,0 +1,14 @@
+package ai0319;
+
+public class VariableTest2 {
+    public static void main(String[] args) {
+        int num1, num2, result;
+
+        num1 = 300;
+        num2 = 500;
+        result = num1 + num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + result);
+        System.out.printf("%d + %d = %d", num1, num2, result);
+    }
+}
